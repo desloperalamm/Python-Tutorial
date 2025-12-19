478e8c8541b0acf0066e39b8256f0f0e0dc5506d
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2025-12-19 18:56:54 +0530

    add both

---
+++ class1.py
@@ -0,0 +1,42 @@
+print("Hello World")
+
+# this is a single line-comment 
+
+
+"""
+this is a multi-line comment
+this is a multi-line comment    
+"""
+
+
+# variables are used to store data in a program
+
+a = 10 # integer number
+b = 20.5 # float number
+c = 3j # complex number
+
+# to check the data type of a variable, we use the type() function
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# Naming Conventions: 
+
+# camelCase = "this is a camel case variable"
+# PascalCase = "this is a pascal case variable"
+# snake_case = "this is a snake case variable"
+
+firstName = "alam"
+LastName = "saif"
+full_name = firstName + " " + LastName
+print(full_name)
+
+
+
+
+
+
+
+
