14b15ff0bbe1d71ea741997ec68bc1a3833ab61a
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2025-12-20 22:51:42 +0530

    add new

---
+++ class3.py
@@ -0,0 +1,29 @@
+# a = "Alam Saif"
+
+# # string slicing
+# # print(a[-1], a[3])
+
+# print(a[0:4])
+
+
+# b = "Hello I am a Data Scientist"
+
+# print(b[13::])
+
+
+# # print statements ways
+
+# name = "Alam Saif"
+# age = 20
+# des = "Data Scientist"
+
+# print("My name is", name, "and I am", age, "years old and I am a", des)
+# print(f"My name is {name} and I am {age} years old and I am a {des}")
+
+# name = input("enter your name: ")
+# age = int(input("enter your age: "))
+# des = input("enter your des: ")
+
+# print(f"My name is {name} and I am {age} years old and I am a {des}")
+
+# print(type(age))
