0d8a4678c38ceff147ad9a6109b832fee1740e42
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2025-12-25 18:03:10 +0530

    add new data

--- class6.py
+++ class6.py
@@ -84,29 +84,95 @@
 # ======================== Question 6th
 # ======================== check a Days of week checker
 
-day = int(input("enter a number:"))
-day = day % 7
+# day = int(input("enter a number:"))
+# day = day % 7
 
-if day == 1:
-    print("Sunday")
+# if day == 1:
+#     print("Sunday")
 
-elif day == 2:
-    print("Monday")
+# elif day == 2:
+#     print("Monday")
 
-elif day == 3:
-    print("Tuesday")
+# elif day == 3:
+#     print("Tuesday")
 
-elif day == 4:
-    print("Wednesday")
+# elif day == 4:
+#     print("Wednesday")
 
-elif day == 5:
-    print("Thursday")
+# elif day == 5:
+#     print("Thursday")
 
-elif day == 6:
-    print("Friday")
+# elif day == 6:
+#     print("Friday")
 
-elif day == 7:
-    print("Saturday")
+# elif day == 7:
+#     print("Saturday")
+
+# else:
+#     print("Please enter a valid number")
+
+
+# Question 7th
+# ======================== check a number three number is equal or not and check which number is greater
+
+# a = int(input("enter first number:"))
+# b = int(input("enter second number:"))
+# c = int(input("enter third number:"))
+
+# if a == b and b == c:
+#     print("All numbers are equal")
+
+# elif a == b or b == c or c == a:
+#     print("Two numbers are equal")
+
+# elif a > b and a > c:
+#     print(f"{a} is greater than {b} and {c}")
+
+# elif b > a and b > c:
+#     print(f"{b} is greater than {a} and {c}")
+
+# else:
+#     print(f"{c} is greater than {a} and {b}")
+
+
+
+# Qustion 8th
+# ======================== check a leap year or not with century
+
+# year = int(input("Enter a year: "))
+
+# if year % 100 == 0 and year % 400 == 0:
+#      print(f"{year} is a leap year")
+
+# elif year % 100 != 0 and year % 4 == 0:
+#      print(f"{year} is a leap year")
+
+# else:
+#      print(f"{year} is not a leap year")
+
+
+# Question 9th
+# ======================== give discount to a person on his total bill amount
+
+# bill = int(input("Enter your total bill amount: "))
+
+# if bill >= 1000 and bill <= 4999:
+#     print(f"Your total bill amount is {bill} and you will get 10% discount and your total bill is {(bill*90)/100}")
+
+# elif bill >= 5000:
+#     print(f"Your total bill amount is {bill} and you will get 20% discount and your total bill is {(bill*80)/100}")
+
+# else:
+#     print(f"Your total bill amount is {bill} and you will not get any discount and your total bill is {bill}")
+
+
+# Question  10th
+# ======================== check character is vowel or consonant
+
+char = input("Enter a Character:")
+
+if char in "aeiouAEIOU":
+    print(f"{char} is a vowel")
 
 else:
-    print("Please enter a valid number")
+    print(f"{char} is a consonant")
