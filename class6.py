a60bfc56838b9c89d4f4fe1e785fc6cabc607a96
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2025-12-25 14:42:37 +0530

    add new data

--- class6.py
+++ class6.py
@@ -6,5 +6,23 @@
 # else:
 #     statement
 
+# age  = int(input("enter your age:"))
+# if age >= 18:
+#     print("you are elder")
 
-print("Hello World")
+# else:
+#     print("you are child")
+
+
+paisa = int(input("enter your paisa:"))
+
+if paisa == 50:
+    print("I can eat only veg biryani")
+
+elif paisa == 100:
+    print("I can eat only biryani")
+
+else:
+    print("I can eat chicken changezi")
+
+    
