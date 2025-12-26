487ed20657822373e6ecf608685ad5ccb32ed3a9
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2025-12-26 21:19:27 +0530

    add new

--- class7.py
+++ class7.py
@@ -1,5 +1,28 @@
 # class 7th
 # =================== loops ==================
 
-for i in range(10,41,1):
-    print("hello python",i)
+# for i in range(10,41,1):
+#     print("hello python",i)
+
+# for i in range(-10,21,1):
+#     print(i)
+
+# for i in range(35,4,-1):
+#     print(i)
+
+
+# =================== print table by coder
+# for i in range(5,51,5):
+#     print(i)
+
+# =================== print table by user input
+
+# n = int(input("Enter a number: "))
+
+# for i in range(n,(n*10)+1,n):
+#     print(i)
+
+# s = "Hello welcome to python"
+
+# for i in range(0,len(s),1):
+#     print(s[i])
