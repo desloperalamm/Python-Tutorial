5591781066735d557b25f96283984c016b551054
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2026-01-07 21:00:45 +0530

    add new class

---
+++ class9.py
@@ -0,0 +1,16 @@
+# def Greet(): # function degining
+    # print("Hello, welcome to class 9!")
+
+# calling the function
+# Greet()
+
+# return statement in function
+
+# def Greet():
+#     return "Hello welcome to return statement"
+
+# print(Greet())
+        
+# ================================================ parameters and arguments
+# =========================================================================
+
