17ccd30088722b5b206e1a50cbfaffccac2ad4b2
Mohammad-Alam-01 <1111mohammadalam@gmail.com>
2026-01-17 21:17:18 +0530

    add new class , data

---
+++ class12.py
@@ -0,0 +1,12 @@
+# # tuple has hytrogenius nature
+
+tuple = (10,20,30,40,50,60,70,80,90,100)
+print(type(tuple))
+print(tuple)
+
+
+# # tuple has immutable nature
+# # we can store same value mutiple time in tuple
+
+
+
